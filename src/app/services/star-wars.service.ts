fc6af5f685b6993ff17e77fb18552465bc13eaca
Aleksandar Mihajlovski <aleksandar.mihajlovski@polarcape.com>
2020-09-16 21:42:50 +0200

    updated dependencies and api url

--- src/app/services/star-wars.service.ts
+++ src/app/services/star-wars.service.ts
@@ -1,69 +1,75 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { mergeMap, toArray, map } from 'rxjs/operators';
-import { from, Observable, forkJoin } from 'rxjs';
-import { IStarWarsResponse } from './models/serviceResponse';
-import { IStarWarsPlanet } from './models/planet';
-import { IStarWarsCharacter } from './models/character';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { forkJoin, from, Observable } from "rxjs";
+import { map, mergeMap, toArray } from "rxjs/operators";
+import { IStarWarsCharacter } from "./models/character";
+import { IStarWarsPlanet } from "./models/planet";
+import { IStarWarsResponse } from "./models/serviceResponse";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class StarWarsService {
-  url = 'https://swapi.co/api';
+  url = "https://swapi.dev/api";
 
-  constructor(private readonly http: HttpClient) { }
+  constructor(private readonly http: HttpClient) {}
 
-  public getPeople(page: number = 1, search: string): Observable<IStarWarsResponse> {
+  public getPeople(
+    page: number = 1,
+    search: string
+  ): Observable<IStarWarsResponse> {
     return this.http
-    .get<IStarWarsResponse>(`${this.url}/people`, {
-      params: {
-        page: page.toString(),
-        search
-      }
-    })
-    .pipe(
-      mergeMap((people: IStarWarsResponse) =>
-        from(people.results).pipe(
-          mergeMap(
-            (character: IStarWarsCharacter) => this.http.get(character.homeworld),
-            (character: IStarWarsCharacter, planet: IStarWarsPlanet) => ({ ...character, planet })
-          ),
-          toArray(),
-          map(results => ({ ...people, results})),
-        )
-      ),
-    );
-  }
-
-  public getCharacter(id: string): Observable<IStarWarsCharacter> {
-    return this.http
-      .get<IStarWarsCharacter>(`${this.url}/people/${id}`)
+      .get<IStarWarsResponse>(`${this.url}/people`, {
+        params: {
+          page: page.toString(),
+          search,
+        },
+      })
       .pipe(
-        mergeMap((result: IStarWarsCharacter) =>
-          from([result]).pipe(
+        mergeMap((people: IStarWarsResponse) =>
+          from(people.results).pipe(
             mergeMap(
-              (character: IStarWarsCharacter) => this.http.get(character.homeworld),
-              (character: IStarWarsCharacter, planet: IStarWarsPlanet) => ({ ...character, planet })
+              (character: IStarWarsCharacter) =>
+                this.http.get(character.homeworld),
+              (character: IStarWarsCharacter, planet: IStarWarsPlanet) => ({
+                ...character,
+                planet,
+              })
             ),
-            mergeMap(
-              character => {
-                let allResidents = character.planet.residents.map((url: string) => this.http.get(url));
-                return forkJoin(...allResidents).pipe(
-                  map((idDataArray) => {
-                    character.planet.residents.forEach((eachResident, index) => {
-                      character.planet.residents[index] = idDataArray[index];
-                    });
-                    return character;
-                  }),
-                );
-              }
-            ),
+            toArray(),
+            map((results) => ({ ...people, results }))
           )
-        ),
+        )
       );
   }
 
+  public getCharacter(id: string): Observable<IStarWarsCharacter> {
+    return this.http.get<IStarWarsCharacter>(`${this.url}/people/${id}`).pipe(
+      mergeMap((result: IStarWarsCharacter) =>
+        from([result]).pipe(
+          mergeMap(
+            (character: IStarWarsCharacter) =>
+              this.http.get(character.homeworld),
+            (character: IStarWarsCharacter, planet: IStarWarsPlanet) => ({
+              ...character,
+              planet,
+            })
+          ),
+          mergeMap((character) => {
+            let allResidents = character.planet.residents.map((url: string) =>
+              this.http.get(url)
+            );
+            return forkJoin(...allResidents).pipe(
+              map((idDataArray) => {
+                character.planet.residents.forEach((eachResident, index) => {
+                  character.planet.residents[index] = idDataArray[index];
+                });
+                return character;
+              })
+            );
+          })
+        )
+      )
+    );
+  }
 }
-
-
